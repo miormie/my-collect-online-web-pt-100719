b41f70032b565d57d83ceeb77c35a123867fb320
Rose Seraphin <roseseraphin@yahoo.com>
2019-10-27 23:03:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,20 +4,20 @@ i = 0
 
   collection = [] 
 
-  while i < array.length 
+#   while i < array.length 
 
-  collection << yield(array[i]) 
+#   collection << yield(array[i]) 
 
-    i += 1 
+#     i += 1 
 
-  end 
+#   end 
 
-collection 
-
-end 
-
+# collection 
 
 end 
 
-my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"] { |name| "Hi, #{name}" }
+
+# end 
+
+# my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"] { |name| "Hi, #{name}" }
 
