f11003928cb35ab0147c8d354a73e71ecac2977d
Rose Seraphin <roseseraphin@yahoo.com>
2019-10-28 00:06:17 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
+def my_collect(array) 
 
+i = 0 
+
+  collection = [] 
+
+  while i < array.length 
+
+  collection << yield(array[i]) 
+
+    i += 1 
+
+  end 
+
+collection 
+
+end 
+
+
+# end 
+
+# my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"] { |name| "Hi, #{name}" }
 
